69ae29971d3b38165692459d75b2aee74a80adb6
lhz <lhz@private.cn>
2024-04-05 20:40:50 +0800

    项目初始化

---
+++ src/main/java/com/authine/lhz/qingming_exam/QingmingExamApplication.java
@@ -0,0 +1,13 @@
+package com.authine.lhz.qingming_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingmingExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QingmingExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/authine/lhz/qingming_exam/QingmingExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authine.lhz.qingming_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QingmingExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
